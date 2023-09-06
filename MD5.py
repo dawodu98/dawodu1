ba7faf8d25e9eea7b84e701f63fa2bcd597918f2
mac <nanudawodu@gmail.com>
2023-09-06 05:18:57 +0100

    added

--- MD5.py
+++ MD5.py
@@ -6,10 +6,10 @@ def generate_sha256(input):
     return sha256.hexdigest()
 
 
+input = "chinwe "
+display_sha256 = generate_sha256(input)
+print(f"sha256 Hash: {display_sha256}")
 
-input = "gomycode"
-sha256_show = generate_sha256(input)
-print(f"sha256 Hash: {sha256_show}")
-
-
+#ebb17344b09005b2bfe462d07256a4a61988c2a792cc8ed10828481eca6b8933 chinwe without space
+#95dd31e2d303f62b8f4756115b1325da5c6c5a03cda3c730c0afbe4159a06ac0 chinwe with space
 
